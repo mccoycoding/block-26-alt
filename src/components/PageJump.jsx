d1a80df480ca39846dfd5d7646c497a8d03f1a04
Mason McCoy <mccoymas@amazon.com>
2023-07-07 02:46:08 -0700

    fixed issue where list would disappear if page jump target === current offset

--- src/components/PageJump.jsx
+++ src/components/PageJump.jsx
@@ -1,11 +1,11 @@
-export default function PageJump({ setPokeOffset, setIsLoading }) {
+export default function PageJump({ setPokeOffset, setIsLoading, pokeOffset }) {
     return (
         <td colSpan={4}>
             <form className="input-group mb-3" action="" onSubmit={(e) => e.preventDefault()}>
                 <input className="form-control" id="jump-input" type="number" placeholder="1-100" min={0} max={100} aria-label="Target page" aria-describedby="jump-submit"/>
                 <button id="jump-submit" className="btn btn-primary" onClick={()=>{
                     const target = document.getElementById("jump-input").value * 10;
-                    if (target > 1000 || target < 0) return 
+                    if (target > 1000 || target < 0 || target !== pokeOffset) return 
                     setIsLoading(true)
                     setPokeOffset(target)
                 }}>Jump</button>
